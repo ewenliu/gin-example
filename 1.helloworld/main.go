aa85e3180724aebc22a10ac4772d2724af65fa56
yiwenliu <yiwenliu@91jkys.com>
2021-12-06 17:36:49 +0800

    ADD hello world

--- 1.helloworld/main.go
+++ 1.helloworld/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func helloWorld(c *gin.Context) {
+	res := gin.H{
+		"message": "hello world!",
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", helloWorld)
+	_ = r.Run("127.0.0.1:8888")
+}
