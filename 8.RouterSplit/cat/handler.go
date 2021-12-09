e451dcd478c7f22e9b458b797862043947b95c3c
yiwenliu <yiwenliu@91jkys.com>
2021-12-09 14:12:14 +0800

    ADD 路由带:xx参数

--- 8.RouterSplit/cat/handler.go
+++ 8.RouterSplit/cat/handler.go
@@ -1,12 +1,18 @@
 package cat
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func barkHandler(c *gin.Context) {
 	c.String(http.StatusOK, "this is cat bark handler")
+}
+
+func infoHandler(c *gin.Context){
+	id := c.Param("id")
+	fmt.Println(id)
 }
 
 func otherHandler(c *gin.Context) {
